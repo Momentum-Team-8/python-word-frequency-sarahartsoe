72ff372aac22d4b4ed4abd4943b151f01a6d5358
sarahartsoe <sarahartsoe@gmail.com>
2021-06-09 15:36:25 -0400

    removed punctuation

--- word_frequency.py
+++ word_frequency.py
@@ -1,6 +1,6 @@
 # from os import read
 
-
+import string
 from typing import Text
 
 
@@ -15,12 +15,12 @@ STOP_WORDS = [
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
     with open(file) as one_today:
-        text = one_today.read()
-        print(repr(text[0:-1]))
+        text = one_today.readlines()
     d = dict()
     for line in text:
         line = line.strip()
         line = line.lower()
+        line = line.translate(line.maketrans("", "", string.punctuation))
         words = line.split(" ")
         for word in words:
             if word in d:
@@ -29,7 +29,7 @@ def print_word_freq(file):
                 d[word] = 1
     for word in list(d.keys()):
         print(word, ":", d[word])
-        print(line)
+    
     
 # ignore stop words
 # count frequency of each word
